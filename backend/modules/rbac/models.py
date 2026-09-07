9a201d43ce70093b97d6777c66708936964a6a4c
Donal Raphel <donalrapheldrk@gmail.com>
2026-09-07 16:14:53 +0530

    feat: add permissions

--- backend/modules/rbac/models.py
+++ backend/modules/rbac/models.py
@@ -10,3 +10,19 @@ class Role(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Permissions(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    codename = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+        
