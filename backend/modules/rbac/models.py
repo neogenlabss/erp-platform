39a8f18a1faee0582780e950adcb08014f462e9c
Donal Raphel <donalrapheldrk@gmail.com>
2026-09-08 10:34:51 +0530

    feat: add many-to-many relationships for roles and permissions

--- backend/modules/rbac/models.py
+++ backend/modules/rbac/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+
 class Role(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
+    permissions = models.ManyToManyField("Permissions", blank=True, related_name="role")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
 
 
 class Permissions(models.Model):
@@ -24,5 +25,3 @@ class Permissions(models.Model):
 
     def __str__(self):
         return self.name
-
-        
